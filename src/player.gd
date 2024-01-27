4d61ac9649eafaf417c90e33220b00aeadf448c2
Jack <blazejmg917@gmail.com>
2024-01-26 22:17:07 -0500

    player

--- src/player.gd
+++ src/player.gd
@@ -1,8 +1,11 @@
 extends CharacterBody2D
-
+var heading = Vector2.ZERO;
 
 const SPEED = 300.0
 
+func _process(delta):
+	var mousePos = get_global_mouse_position();
+	heading = mousePos - global_position;
 
 func _physics_process(delta):
 	var direction = Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down")).normalized()
