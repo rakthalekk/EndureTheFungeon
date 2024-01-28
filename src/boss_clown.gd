3f3b6929cabac56279b951524ba5052f2b76989a
dmglynn <dmglynn@ncsu.edu>
2024-01-27 23:45:27 -0500

    pretty much finished boss

--- src/boss_clown.gd
+++ src/boss_clown.gd
@@ -41,7 +41,7 @@ func sweep():
 	arc()
 	await get_tree().create_timer(1.2).timeout
 	arc()
-	await get_tree().create_timer(1.2).timeout
+	await get_tree().create_timer(3).timeout
 
 
 func arc():
@@ -58,32 +58,74 @@ func arc():
 		make_bullet("BossSweep")
 		await get_tree().create_timer(.2).timeout
 
+
 func split():
 	for i in range(5):
 		fire_direction = (player.global_position - global_position).normalized()
 		make_bullet("BossSplit")
 		await get_tree().create_timer(1).timeout
+	
+	await get_tree().create_timer(2).timeout
 
 
 func wavy():
-	for i in range(30):
+	for i in range(50):
 		fire_direction = Vector2(2, 1).normalized().rotated(deg_to_rad(randi_range(0, 120)))
 		make_bullet("BossWavy")
-		await get_tree().create_timer(.2).timeout
+		await get_tree().create_timer(.15).timeout
+	
+	get_tree().create_timer(2).timeout
 
 
 func wiggly():
-	for i in range(30):
-		fire_direction = Vector2(2, 1).normalized().rotated(deg_to_rad(randi_range(0, 120)))
+	for i in range(5):
+		fire_direction = Vector2(1, 0)
 		make_bullet("BossWiggly")
-		await get_tree().create_timer(.3).timeout
+		
+		for j in range(6):
+			fire_direction = fire_direction.rotated(deg_to_rad(30))
+			make_bullet("BossWiggly")
+		
+		await get_tree().create_timer(0.5).timeout
+		
+		fire_direction = Vector2(1, 0).rotated(deg_to_rad(15))
+		make_bullet("BossWiggly")
+		
+		for j in range(5):
+			fire_direction = fire_direction.rotated(deg_to_rad(30))
+			make_bullet("BossWiggly")
+	
+		await get_tree().create_timer(0.5).timeout
+	
+	await get_tree().create_timer(2).timeout
 
 
 func bouncy():
-	for i in range(30):
+	for i in range(40):
 		fire_direction = Vector2(2, 1).normalized().rotated(deg_to_rad(randi_range(0, 120)))
 		make_bullet("BossBouncy")
-		await get_tree().create_timer(.3).timeout
+		await get_tree().create_timer(.15).timeout
+	
+	await get_tree().create_timer(2).timeout
+
+
+func get_random_attack():
+	var random = randi_range(1, 7)
+	match random:
+		1:
+			await single()
+		2:
+			await burst()
+		3:
+			await sweep()
+		4:
+			await split()
+		5:
+			await bouncy()
+		6:
+			await wiggly()
+		7:
+			await wavy()
 
 
 func make_bullet(bullet_name: String):
@@ -101,3 +143,6 @@ func _on_start_timer_timeout():
 	await bouncy()
 	await wiggly()
 	await wavy()
+	
+	while true:
+		await get_random_attack()
